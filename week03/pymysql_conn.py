e6aba620bd6c4f019f85aa6b9ccfb1a6b3a9cc05
tanlay <18313178130@163.com>
2021-01-04 15:53:32 +0800

    pymysql连接mysql

---
+++ week03/pymysql_conn.py
@@ -0,0 +1,23 @@
+import pymysql as db
+
+HOST = 'localhost'
+USER = 'python'
+PASSWD = 'python123'
+DBNAME = 'testdb'
+PORT = 3308
+
+conn = db.connect(HOST, USER, PASSWD, DBNAME, PORT)
+try:
+    with conn.cursor() as cursor:
+        sql = '''select version()'''
+        # 使用execute方法执行SQL
+        cursor.execute(sql)
+        result = cursor.fetchone()
+    conn.commit()
+except Exception as err:
+    print(f"数据库连接失败")
+finally:
+    # 关闭数据库连接
+    conn.close()
+
+print(f"DB version: {result}")
