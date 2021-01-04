03d961d51d59a6a0869883e4cdc24180621de4fb
tanlay <18313178130@163.com>
2021-01-04 18:30:55 +0800

    sqlchemy ORM+pymysql创建新库表~

---
+++ week03/orm_conn.py
@@ -0,0 +1,28 @@
+import pymysql
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import DateTime
+
+Base = declarative_base()
+
+
+class Book_table(Base):
+    __tablename__ = 'bookorm'
+    book_id = Column(Integer(), primary_key=True)
+    book_name = Column(String(20), index=True)
+
+
+class Author_table(Base):
+    __tablename__ = 'authororm'
+    user_id = Column(Integer(), primary_key=True)
+    username = Column(String(128), nullable=False, unique=True)
+    created_time = Column(DateTime(), default=datetime.now)
+    updated_time = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+
+# 实例化一个引擎
+dburl="mysql+pymysql://python:python123@localhost:3308/testdb?charset=utf8mb4"
+engine=create_engine(dburl, echo=True, encoding="utf-8")
+
+Base.metadata.create_all(engine)
--- week03/sqlalchemy_conn.py
+++ week03/sqlalchemy_conn.py
@@ -5,20 +5,21 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, 
 # 需要创建数据库testdb,并授权新用户python访问
 # mysql> grant all on testdb.* to 'python'@'%' identified by 'python123'
 # echo=True 开启调试
-engine = create_engine("mysql+pymysql://python:python123@localhost:3308/testdb",echo=True)
+engine = create_engine(
+    "mysql+pymysql://python:python123@localhost:3308/testdb", echo=True)
 
 # 创建元数据
 metadata = MetaData(engine)
 
 book_table = Table('book', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String(20)),
-    )
+                   Column('id', Integer, primary_key=True),
+                   Column('name', String(20)),
+                   )
 author_table = Table('author', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('book_id', None, ForeignKey('book.id')),
-    Column('author_name', String(128), nullable=False),
-)
+                     Column('id', Integer, primary_key=True),
+                     Column('book_id', None, ForeignKey('book.id')),
+                     Column('author_name', String(128), nullable=False),
+                     )
 
 try:
     metadata.create_all()
